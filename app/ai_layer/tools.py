5d66d537a35ae7fd037a2a2596026a34a5a051b4
Abdullah-kwl <mrabdullahkwl@gmail.com>
2026-05-05 23:13:49 +0500

    search tool is added

---
+++ app/ai_layer/tools.py
@@ -0,0 +1,5 @@
+from langchain_community.tools import DuckDuckGoSearchRun
+
+search_tool = DuckDuckGoSearchRun()
+# search_results = search_tool.invoke("Obama's first name?")
+# print(search_results)
