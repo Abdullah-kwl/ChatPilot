02f8d84e317b942fc712ef2a161d297348b115f8
Abdullah-kwl <mrabdullahkwl@gmail.com>
2026-05-06 23:55:19 +0500

    added the exception handling

--- app/api/chat.py
+++ app/api/chat.py
@@ -1,13 +1,14 @@
 from fastapi import APIRouter
-from app.api.schemas import Messages, ChatResponse
+from fastapi import HTTPException
 from app.ai_layer.chatbot import agent
 from app.api.helper import to_openai_format
+from app.api.schemas import Messages, ChatResponse
 
 router = APIRouter()
 
 
-@router.post("/chat")
-async def root(message: Messages, response_model=ChatResponse):
+@router.post("/chat", response_model=ChatResponse)
+async def root(message: Messages):
     try:
         results = agent.invoke(
         {"messages": [{"role": "user", "content": message.message}]},
@@ -15,4 +16,4 @@ async def root(message: Messages, response_model=ChatResponse):
         )
         return {"response": to_openai_format(results["messages"])}
     except Exception as e:
-        return {"error": str(e)}
+        raise HTTPException(status_code=500, detail=str(e))
